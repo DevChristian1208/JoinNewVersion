8b1f43e532bb85829b245d199227108f6e4b1fac
Christian <christian.pressig@web.de>
2025-07-06 09:44:23 +0200

    changes

--- js/include.js
+++ js/include.js
@@ -9,7 +9,7 @@ async function includeHTML() {
     let includeElements = document.querySelectorAll('[w3-include-html]');
     for (let i = 0; i < includeElements.length; i++) {
         const element = includeElements[i];
-        file = element.getAttribute("w3-include-html"); // "includes/header.html"
+        file = element.getAttribute("w3-include-html"); 
         let resp = await fetch(file);
         if (resp.ok) {
             element.innerHTML = await resp.text();
@@ -27,7 +27,7 @@ function toggleMenu() {
 
 
 function returnToHome() {
-    window.location.href = "./index.html";
+    window.location.href="./index.html"
 }
 
 function redirectToHelp() {
